f1e3738b8b718d778b1c7967b7ff661994b5ac8b
Nabu-8 <7888nn@gmail.com>
2025-05-10 21:39:13 +0300

    Homework 8

---
+++ lesson_08/homework_08.py
@@ -0,0 +1,21 @@
+# Створіть масив зі строками, які будуть складатися з чисел, які розділені комою. Наприклад:
+# [”1,2,3,4”, ”1,2,3,4,50” ”qwerty1,2,3”]
+# Для кожного елементу списку виведіть суму всіх чисел (створіть нову функцію для цього).
+# Якщо є символи, що не є числами (”qwerty1,2,3” у прикладі), вам потрібно зловити вийняток і вивести “Не можу це зробити!”
+# Використовуйте блок try\except, щоб уникнути інших символів, окрім чисел у списку.
+# Для цього прикладу правильний вивід буде - 10, 60, “Не можу це зробити”
+
+def sum_el_array(array):
+    summ_numbers = []
+    for string in array:
+        string_parts = string.split(',')
+        try:
+            numbers = [int(part) for part in string_parts]
+            summ_numbers.append(sum(numbers))
+        except ValueError:
+            summ_numbers.append('"Не можу це зробити"')
+    return summ_numbers
+
+array = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
+result = sum_el_array(array)
+print(*result, sep=', ')
