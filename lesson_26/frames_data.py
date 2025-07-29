22bba4f48764521fd8caa0a3f3b635d0c00b4496
Nabu-8 <7888nn@gmail.com>
2025-07-29 10:04:00 +0300

    HW23

---
+++ lesson_26/frames_data.py
@@ -0,0 +1,4 @@
+frames_data = [
+    {"frame_id": "frame1", "input_id": "input1", "secret_phrase": "Frame1_Secret"},
+    {"frame_id": "frame2", "input_id": "input2", "secret_phrase": "Frame2_Secret"},
+]
