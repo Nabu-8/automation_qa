153ea6002681250fba89f04708025f3bd81c838d
Nabu-8 <7888nn@gmail.com>
2025-08-01 12:21:55 +0300

    Improvement for HW23 (webdriver to fixture)

--- lesson_26/test_hw_23.py
+++ lesson_26/test_hw_23.py
@@ -8,30 +8,25 @@ from lesson_26.frames_data import frames_data
 
 @pytest.mark.hw23()
 @pytest.mark.parametrize("frame", frames_data)
-def test_frame(frame):
-    driver = webdriver.Chrome()
+def test_frame(driver, frame):
     driver.get("http://localhost:8000/dz.html")
 
-    try:
-        driver.switch_to.frame(driver.find_element(By.ID, frame["frame_id"]))
+    driver.switch_to.frame(driver.find_element(By.ID, frame["frame_id"]))
 
-        input_field = driver.find_element(By.ID, frame["input_id"])
-        input_field.send_keys(frame["secret_phrase"])
-        driver.find_element(By.CSS_SELECTOR, "button").click()
-        time.sleep(1)
-        alert = Alert(driver)
-        assert alert.text == "Верифікація пройшла успішно!"
-        alert.accept()    # ОК to close alert
+    input_field = driver.find_element(By.ID, frame["input_id"])
+    input_field.send_keys(frame["secret_phrase"])
+    driver.find_element(By.CSS_SELECTOR, "button").click()
+    time.sleep(1)
+    alert = Alert(driver)
+    assert alert.text == "Верифікація пройшла успішно!"
+    alert.accept()    # ОК to close alert
 
-        input_field.clear()
-        input_field.send_keys("Some wrong text!")
-        driver.find_element(By.CSS_SELECTOR, "button").click()
-        time.sleep(1)
-        alert = Alert(driver)
-        assert alert.text == "Введено неправильний текст!"
-        alert.accept()
+    input_field.clear()
+    input_field.send_keys("Some wrong text!")
+    driver.find_element(By.CSS_SELECTOR, "button").click()
+    time.sleep(1)
+    alert = Alert(driver)
+    assert alert.text == "Введено неправильний текст!"
+    alert.accept()
 
-        driver.switch_to.default_content()
-
-    finally:
-        driver.quit()
+    driver.switch_to.default_content()
